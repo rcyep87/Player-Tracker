823538ea057c178b678a4e5887e8cab593774582
Ryan Yep <ryan.yep@gmail.com>
2016-01-06 16:40:13 -0600

    Able to search player by first name

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -75,6 +75,6 @@ class PlayersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def player_params
-      params.require(:player).permit(:first_name, :last_name, :DOB, :position, :college, :debut, :bats, :throws, :height)
+      params.require(:player).permit(:first_name, :last_name, :DOB, :position, :college, :debut, :bats, :throws, :height, :search)
     end
 end
